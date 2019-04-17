2d92481ca67b33ab3f2060f6ebc83d15fb79b4eb
SupremeSalvatore <milosposkuricakg@gmail.com>
2019-04-17 22:12:05 +0200

    Updates

---
+++ precache-manifest.d57051338493e9024c41d03fd5a26ca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da3cacbaac6abc8f2070",
+    "url": "/react-fitness-tracker/static/js/runtime~main.4ffd0d3c.js"
+  },
+  {
+    "revision": "26682666e36eeea13839",
+    "url": "/react-fitness-tracker/static/js/main.222d1c28.chunk.js"
+  },
+  {
+    "revision": "5f540fe33fd5cb748fe2",
+    "url": "/react-fitness-tracker/static/js/2.a38ee8fb.chunk.js"
+  },
+  {
+    "revision": "26682666e36eeea13839",
+    "url": "/react-fitness-tracker/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "068c7a6e6b94ac4a8b2527a6a9285602",
+    "url": "/react-fitness-tracker/index.html"
+  }
+];
